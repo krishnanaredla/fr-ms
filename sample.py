f7b4352670a95ddf077ff54b8b8a920a71b836ff
Krishna Reddy <gopalkrishna.naredla@gmail.com>
2023-07-28 10:41:32 -0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ client = boto3.client('sns', endpoint_url='http://localhost:4566')
 message = {"message": "Processing failed with error"}
 try:
     response = client.publish(
-        TargetArn="arn:aws:sns:us-east-1:000000000000:bcbsnc-helix-ingestion-notification",
+        TargetArn="arn:aws:sns:us-east-1:000000000000:alpha-helix-ingestion-notification",
         Message=json.dumps({"email": json.dumps(message)}),
         Subject="File Registration: Error",
         #MessageStructure="json",
